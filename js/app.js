6bc1d0f37bb5b8c5c9756577ea7cf0c4cf03f54f
disc0ninja <disc0ninja77@gmail.com>
2016-03-01 20:34:21 -0600

    Feature: Game Canvas
    * Updated app.js with console.log() statments in order to get game canvas operational and clear errors in js console

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -24,13 +24,27 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+  this.sprite = 'images/char-cat-girl.png';
+};
 
+Player.prototype.update = function(dt) {
+  //console.log("Update");
+};
 
+Player.prototype.render = function() {
+  //console.log("player render");
+};
+
+Player.prototype.handleInput = function() {
+  console.log("handling input");
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+var allEnemies = [new Enemy()];
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
